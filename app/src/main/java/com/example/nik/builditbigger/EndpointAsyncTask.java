15c5bd8f313b80ce6719f5a04a76af45033e50f3
Nikhil B <nkl07ba@gmail.com>
2016-10-28 01:16:33 +0530

    Backend deployed to google appengine
    The app engine was deployed

--- app/src/main/java/com/example/nik/builditbigger/EndpointAsyncTask.java
+++ app/src/main/java/com/example/nik/builditbigger/EndpointAsyncTask.java
@@ -25,20 +25,8 @@ class EndpointAsyncTask extends AsyncTask<Context, Void, String> {
     @Override
     protected String doInBackground(Context... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
-            // end options for devappserver
-
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://builditbigger-147719.appspot.com/_ah/api/");
             myApiService = builder.build();
         }
 
