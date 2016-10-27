b63ceb51f86dc4f6c982e03c5bc0637686ea4fbf
Nikhil B <nkl07ba@gmail.com>
2016-10-27 21:02:51 +0530

    Added backend server, jokeActivity now retrives from the dev server
    A server backend was added it takes data from the java library and then it is displayed with the android library activity

---
+++ backend/src/main/java/com/example/nik/myapplication/backend/MyEndpoint.java
@@ -0,0 +1,43 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.example.nik.myapplication.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.example.JokeClass;
+
+import javax.inject.Named;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.nik.example.com",
+                ownerName = "backend.myapplication.nik.example.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+
+        JokeClass jokeClass = new JokeClass();
+        String joke = jokeClass.getJoke();
+        response.setData(joke);
+        return response;
+    }
+
+}
